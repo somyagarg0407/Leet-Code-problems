44cac0568aefda25b5cf8cde4c3cf43b63118411
Somya Garg <somyagarg0407@gmail.com>
2026-07-18 22:28:30 +0530

    Add files via upload

---
+++ 142_876.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = nullptr;
+    }
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
+
+int main() {
+
+    // 1 -> 2 -> 3 -> 4 -> 5 -> 6
+
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+    head->next->next->next->next->next = new ListNode(6);
+
+    Solution obj;
+
+    ListNode* middle = obj.middleNode(head);
+
+    cout << "Middle Node = " << middle->val << endl;
+}
